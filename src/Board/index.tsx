33f54731fb0a8986d28e30bae220b869f882d13c
Adam Wehunt <adamwehunt@Marisas-MacBook-Pro.local>
2023-04-30 14:54:04 -0700

    Focus board on load.

--- src/Board/index.tsx
+++ src/Board/index.tsx
@@ -1,16 +1,26 @@
-import { For } from 'solid-js';
+import { For, onMount } from 'solid-js';
 import boardState from '../boardState';
 import Row from './Row';
 import { ScaleText } from '../components/ScaleText';
 import styles from './board.module.css';
 
 const Board = () => {
+	let boardRef: HTMLDivElement | undefined;
+	onMount(() => {
+		boardRef?.focus();
+	});
+
 	const { display, score, onKeyDown } = boardState();
 
 	return (
 		<div class={styles.boardWrapper}>
 			<ScaleText className={styles.score} align={'right'} text={score} />
-			<div class={styles.board} tabIndex={0} onKeyDown={onKeyDown}>
+			<div
+				ref={boardRef}
+				class={styles.board}
+				tabIndex={0}
+				onKeyDown={onKeyDown}
+			>
 				<For each={display()}>{(row) => <Row row={row} />}</For>
 			</div>
 			<div class={styles.boardShadow} />
