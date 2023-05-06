4004d80a7062d4125ab0c08fff793fc9b97b79e7
Adam Wehunt <adamwehunt@Marisas-MacBook-Pro.local>
2023-05-05 17:45:40 -0700

    Add vercel analytics.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import type { Component } from 'solid-js';
 import { COLUMN_COUNT, ROW_COUNT } from './tetris';
 import Board from './Board';
+import { inject } from '@vercel/analytics';
 import styles from './app.module.css';
+
+inject();
 
 const App: Component = () => (
 	<main
